a2bb1fd155652c3075b91b43486347f0d47a1425
Ashish Chauhan <iashishsinghchauhan@gmail.com>
2026-06-17 01:51:31 +0530

    - Added UseStateHook

--- src/components/UseStateHook.jsx
+++ src/components/UseStateHook.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable react-native/no-inline-styles */
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native'
+import React, { useState } from 'react'
 
 const UseStateHook = () => {
-  return (
-    <View>
-      <Text style={ {fontSize: 30} }>UseStateHook</Text>
-    </View>
-  )
+    // Array de-structuring concept here:
+    // useState() -> gives two things: 1. variable name to observe 2. setter to update the value and UI
+    // Default value of variable can be passed to useState hook like below:
+    const [name, setName] = useState('Ashish')
+    const updateName = () => {
+        setName('Chauhan')
+    }
+    return (
+        <View>
+            <Text style={{ fontSize: 30 }}>UseStateHook</Text>
+            <Text style={{ fontSize: 24 }}>{name}</Text>
+            {/* <Button title='Press to change name' onPress={ () => setName('Chauhan') }/> */}
+            <Button title='Press to change name' onPress={updateName}/>
+        </View>
+    )
 }
 
 export default UseStateHook
