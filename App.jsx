d0f2c3527293cef3b9892a4fd46b98fb8ce0977b
Ashish Chauhan <iashishsinghchauhan@gmail.com>
2026-06-27 14:40:31 +0530

    Added POST component

--- App.jsx
+++ App.jsx
@@ -24,7 +24,8 @@ import { StyleSheet } from 'react-native';
 // import UseRefHook from './src/components/UseRefHook';
 // import ModalDialogBox from './src/components/ModalDialogBox';
 // import AlertExample from './src/components/AlertExample';
-import GET_API from './src/components/GET_API';
+// import GET_API from './src/components/GET_API';
+import POST_API from './src/components/POST_API';
 
 const App = () => {
   return (
@@ -54,7 +55,8 @@ const App = () => {
         {/* <UseRefHook /> */}
         {/* <ModalDialogBox /> */}
         {/* <AlertExample /> */}
-        <GET_API />
+        {/* <GET_API /> */}
+        <POST_API />
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ src/components/POST_API.tsx
@@ -0,0 +1,46 @@
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const POST_API = () => {
+  const [formData, setFormData] = useState({ id: '', name: '', email: '' });
+  const handleInput = (field, value) => {
+    setFormData({ ...formData, [field]: value });
+  };
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/users',
+        formData,
+      );
+      console.log(response.data);
+      Alert.alert('Success!', 'Data submitted successfully');
+      setFormData({ id: '', name: '', email: '' });
+    } catch (error) {
+      Alert.alert('Error!', 'Failed to submit data');
+    }
+  };
+  return (
+    <View>
+      <Text>POST_API</Text>
+      <TextInput
+        placeholder="Enter id"
+        value={formData.id}
+        onChangeText={value => handleInput('id', value)}
+      />
+      <TextInput
+        placeholder="Enter name"
+        value={formData.name}
+        onChangeText={value => handleInput('name', value)}
+      />
+      <TextInput
+        placeholder="Enter email"
+        value={formData.email}
+        onChangeText={value => handleInput('email', value)}
+      />
+      <Button title="Submit" onPress={() => handleSubmit()} />
+    </View>
+  );
+};
+
+export default POST_API;
